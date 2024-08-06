daa01c1617858ff768564614c7570e354a6c10fb
Vishal Sharma <sharmavs9205@gmail.com>
2024-08-07 00:56:16 +0530

    flash coint implementation

---
+++ tronbox-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  networks: {
+    development: {
+      privateKey: '',
+      userFeePercentage: 100,
+      feeLimit: 1e8,
+      fullHost: 'http://127.0.0.1:9090',
+      network_id: '9'
+    },
+    shasta: {
+      privateKey: '',
+      userFeePercentage: 50,
+      feeLimit: 1e8,
+      fullHost: 'https://api.shasta.trongrid.io',
+      network_id: '2'
+    },
+    mainnet: {
+      privateKey: '',
+      userFeePercentage: 100,
+      feeLimit: 1e8,
+      fullHost: 'https://api.trongrid.io',
+      network_id: '1'
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+};
